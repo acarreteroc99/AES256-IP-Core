87a0df645f32ff9591e8f0314e42d66855fedcf0
acarreteroc99 <acarreteroc99@gmail.com>
2021-09-13 18:18:14 +0200

    Updating all files and data after today's meeting

--- AES-256/TFG_mainAES256.c
+++ AES-256/TFG_mainAES256.c
@@ -27,7 +27,7 @@ int main(){
   */
 
   //Poner siempre '<' para que el bucle cuente el size
-  unsigned char *aux = (unsigned char *) "Hola Mundo<";
+  unsigned char *aux = (unsigned char *) "000000000000000000000000<";
 
   while(aux[size] != '<'){
     text[size] = aux[size];
@@ -35,7 +35,8 @@ int main(){
   }
 
 
-  key = (unsigned char *) "QfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s5";
+  //key = (unsigned char *) "QfTjWnZr4u7x!A%D*G-KaPdSgUkXp2s5";
+  key = (unsigned char *) "000102030405060708090a0b0c0d0e0f";
 
   printf("Texto introducido: %s \n", text);
 
