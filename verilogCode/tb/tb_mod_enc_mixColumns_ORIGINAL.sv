f00bf5779f780e923d03d2db1db20f4265396cd2
acarreteroc99 <acarreteroc99@gmail.com>
2021-10-17 18:26:30 +0200

    Problem with reg163 and ROM solved: delaying the wr_reg162 was needed + discorvered that mixColumns that I have been using so far didn't work properly, therefore had to code one entirely

--- verilogCode/tb/tb_mod_enc_mixColumns_ORIGINAL.sv
+++ verilogCode/tb/tb_mod_enc_mixColumns_ORIGINAL.sv
@@ -1,25 +1,26 @@
 
-//`include "../design/enc/mod_enc_mixColumns.sv"
+`include "../design/discarded/mod_enc_mixColumns_ORIGINAL.sv"
 
 `timescale 1ns/10ps     // time-unit = 1 ns, precision 10 ps
 
-module tb_mod_enc_mixColumns();
+module tb_mod_enc_mixColumns_OR();
 
     localparam N = 16;
     localparam period = 20;
 
-    reg clk, rst, wr_en;
-    reg [(N-1):0][7:0] i;
+    reg clk, rst, enable;
+    reg [127:0] i;
 
-    wire [(N-1):0][7:0] o;
+    wire [127:0] o;
+    wire done;
 
     genvar j;
     integer index;
 
-    mod_enc_mixColumns DUT(
-                        .clk(clk), .resetn(rst),
-                        .state(i), .wr_en(wr_en),
-                        .state_out(o)
+    mod_enc_mixColumns_ORIGINAL DUT(
+                        .clk(clk), .reset(rst), .enable(enable),
+                        .state(i), 
+                        .state_out(o), .done(done)
                         );
 
     always #10 clk = !clk;
@@ -27,8 +28,8 @@ module tb_mod_enc_mixColumns();
     // -------- Init input matrix ---------
     initial 
     begin
-        $dumpfile("wv_mod_enc_mixColumns.vcd");
-        $dumpvars(0, tb_mod_enc_mixColumns);
+        $dumpfile("wv_mod_enc_mixColumns_OR.vcd");
+        $dumpvars(0, tb_mod_enc_mixColumns_OR);
 
         //i = {8'h63, 8'h53, 8'he0, 8'h8c, 8'h09, 8'h60, 8'he1, 8'h04, 8'hcd, 8'h70, 8'hb7, 8'h51, 8'hba, 8'hca, 8'hd0, 8'he7};
 
@@ -79,16 +80,13 @@ module tb_mod_enc_mixColumns();
     initial 
     begin
         clk = 1'b0;
-        rst = 1'b0;
-        #period rst = 1'b1;
+        rst = 1'b1;
+        #period rst = 1'b0;
 
-        wr_en = 1'b1;
-
-        i[0] = 8'hab; i[1] = 8'hab; i[2] = 8'hd7;    i[3] = 8'hfe;
-        i[4] = 8'h67; i[5] = 8'h01; i[6] = 8'h30;    i[7] = 8'h01;
-        i[8] = 8'h6b; i[9] = 8'hf2; i[10] = 8'hf2;   i[11] = 8'h6f;
-        i[12] = 8'h63; i[13] = 8'hc5; i[14] = 8'h77;   i[15] = 8'h7c;
+        enable = 1'b1;
         
+        i = 128'he7d0caba51b770cd04e160098ce05363;
+
         /*
         i[0] = 8'h63;
         i[1] = 8'h53;
@@ -108,53 +106,41 @@ module tb_mod_enc_mixColumns();
         i[15]  = 8'he7;
         */
         
-        //reg161_full = 1'b1;
         
         @(posedge clk)
         @(posedge clk)
         @(posedge clk)
-        //reg161_full = 1'b0;
-        
-        //reg162_full = 1'b0;
-        
-        $display("Regular case: reg161 full and reg162 empty \n");
-
-        for(index = 0; index < 10; index = index+1)
-            @(posedge clk)
+        @(posedge clk)
 
         test_mixColumns;
+
+        /*    
+        $display("NOT Regular case: reg161 full and reg162 full \n");
         
-        //@(posedge clk)
-        //reg161_full = 1'b0;
-        
-        
-        for(index = 0; index < 10; index = index+1)
-            @(posedge clk)
-            
-         $display("NOT Regular case: reg161 full and reg162 full \n");
-        
-        /*
         for(index = 0; index < N; index = index+1)
             i[index] = 8'h01;
-        */
-         
-        //@(posedge clk)
-        //reg161_full = 1'b1;
-        
+
         @(posedge clk)
-        //reg162_full = 1'b1;
+        @(posedge clk)
+        @(posedge clk)
+        @(posedge clk)
+        @(posedge clk)
         
         test_mixColumns;
         
         $display("NOT Regular case: reg161 empty and reg162 empty \n");
         
-        /* 
         for(index = 0; index < N; index = index+1)
             i[index] = 8'h02;
-        */
          
-        
+        @(posedge clk)
+        @(posedge clk)
+        @(posedge clk)
+        @(posedge clk)
+        @(posedge clk)
+
         test_mixColumns;
+        */
         
         $finish;
         
