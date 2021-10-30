c75e2fc523ae0270ba6cca75628f282688887bbb
Adrián Carretero Canut <55785663+acarreteroc99@users.noreply.github.com>
2021-10-30 10:36:06 +0300

    TFG_VerilogCode deleted
    Test directory. No longer needed

--- TFG_VerilogCode/test/obj_dir/tb_alu.d
+++
@@ -1,4 +0,0 @@
-tb_alu.o: ../tb_alu.cpp /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_vcd_c.h \
- /usr/share/verilator/include/verilated.h Valu.h Valu___024unit.h
